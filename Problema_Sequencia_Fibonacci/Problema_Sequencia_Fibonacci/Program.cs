bbcc2b28a71df30a0699b7fcbc46c1501ffd4416
samuellucas866 <samuellucas188@gmail.com>
2024-03-22 09:59:36 -0300

    Projetos Enviados
    Códigos de resolução de case, para verificação.

---
+++ Problema_Sequencia_Fibonacci/Problema_Sequencia_Fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um número inteiro positivo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Verifica se o número pertence ou não a sequência de Fibonacci.
+            bool sequencia = VerificaSePertenceFibonacci(n);
+
+            if (sequencia)
+                Console.WriteLine($"O número {n} pertence à sequência de Fibonacci.");
+            else
+                Console.WriteLine($"O número {n} não pertence à sequência de Fibonacci.");
+
+
+        }
+
+        //método booleano para ver se a soma do primeiro termo e segundo chega ao valor de n
+        //Pois a soma do primeiro e segundo termo, tem que fazer parte da sequência até n.
+        static bool VerificaSePertenceFibonacci(int x)
+        {
+            int a = 0;
+            int b = 1;
+
+            while (b <= x)
+            {
+                if (b == x)
+                    return true;
+
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Problema_Soma/Problema_Soma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            int indice = 13, soma =0,k=0;
+
+            while(k < indice)
+            {
+                k= k+1;
+
+                soma = soma + k;
+            }
+
+            Console.Write("Soma = "+soma);
+            
+
+        }
+    }
+}
