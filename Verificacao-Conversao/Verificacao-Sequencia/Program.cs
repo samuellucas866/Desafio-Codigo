df6e9d1e8439c674435621d5cf0f0a9f0d0f5b1a
samuellucas866 <samuellucas188@gmail.com>
2024-03-22 10:30:31 -0300

    Novo commit
    Códigos faltantes

---
+++ Verificacao-Conversao/Verificacao-Sequencia/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            string formataString = "Nome";
+
+            string invercaoString = InverteString(formataString);
+
+            Console.WriteLine("String original = " + formataString);
+
+            Console.WriteLine("String invertida = " + invercaoString);
+
+
+            static string InverteString(string formataString)
+            {
+                char[] caractere = formataString.ToCharArray();
+
+                int tipo = caractere.Length;
+
+                char[] inverte = new char[tipo];
+
+                for (int i = 0; i < tipo; i++)
+                {
+                    inverte[i] = caractere[tipo - 1 - i];
+                }
+
+                return new string(inverte);
+            }
+        }
+    }
+}
---
+++ Verificacao-Sequencia/Verificacao-Sequencia/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace Programa
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+
+            Console.WriteLine("Digite uma sequência de números separados por vírgula: ");
+            string input = Console.ReadLine();
+
+            int[] num = ParseInput(input);
+
+            bool aumentaDeDois = VerificaSeAumenta(num);
+
+            if (aumentaDeDois)
+            {
+                Console.WriteLine("A sequência aumenta de 2 em 2");
+            }
+            else
+            {
+                Console.WriteLine("A sequência não aumenta de 2 em 2");
+            }
+
+        }
+
+        //Método para que remova os espaços em brancos e verifique os números entre as vírgulas.
+        static int[] ParseInput(string input)
+        {
+            return input.Split(',')
+                        .Select(s => int.Parse(s.Trim()))
+                        .ToArray();
+        }
+
+        //Método para verificar se a ordem/sequência dos números estão aumentando de
+        //2 em 2 ou não estão aumentando de 2 em 2.
+        static bool VerificaSeAumenta(int[] numeros)
+        {
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] != numeros[i - 1] + 2)
+                    return false;
+            }
+            return true;
+        }
+
+
+    }
+
+
+}
+
